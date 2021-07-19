7f2191b33a1612b68ad6a636a7c13f67c8ce3aba
brucemoran <bruce01campus@gmail.com>
2021-07-20 00:29:07 +0100

    multiple genes; palette colouring

--- R/run_rpart.R
+++ R/run_rpart.R
@@ -1,7 +1,7 @@
 #' Run rpart on a named gene, return clinical data with appended high/low based on cutoff
 #'
 #' @param expr_df data.frame containing expression in log2tpm with a unique column represented by param gene_id, rows are samples
-#' @param gene_id string in colnames(expr_df) relating to log2tpm data
+#' @param gene_ids vector of strings in colnames(expr_df) relating to log2tpm data for one or more gene
 #'
 #' @param clin_df data.frame containing clinical data with unique columns represented by surv_event and surv_time, rows are samples
 #' @param surv_event colnames(clin_df) relating to survival event
@@ -18,14 +18,14 @@
 #'
 #' @export
 
-run_rpart <- function(expr_df, gene_id, clin_df, surv_event, surv_time, join_el = NULL){
+run_rpart <- function(expr_df, gene_ids, clin_df, surv_event, surv_time, join_el = NULL){
 
   ##parse relevant columns from inputs
   if(is.null(join_el)){
-    expr_tb <- tibble::as_tibble(expr_df[,gene_id], rownames = "sample")
+    expr_tb <- tibble::as_tibble(expr_df[,gene_ids], rownames = "sample")
     surv_tb <- tibble::as_tibble(clin_df[,surv_event], rownames = "sample")
   } else {
-    expr_tb <- tibble::as_tibble(expr_df[,c(join_el, gene_id)])
+    expr_tb <- tibble::as_tibble(expr_df[,c(join_el, gene_ids)])
     colnames(expr_tb)[colnames(expr_tb) == join_el] <- "sample"
     surv_tb <- tibble::as_tibble(clin_df[,c(join_el, surv_event)])
     colnames(surv_tb)[colnames(surv_tb) == join_el] <- "sample"
@@ -33,21 +33,42 @@ run_rpart <- function(expr_df, gene_id, clin_df, surv_event, surv_time, join_el 
 
   #join and fit-tree
   surv_expr_tb <- dplyr::left_join(surv_tb, expr_tb, by = "sample")
-  colnames(surv_expr_tb) <- c("sample", surv_event, "gene")
-  s_expr <- as.data.frame(surv_expr_tb[,c(surv_event, "gene")])
-  fit_tree <- rpart::rpart(s_expr, method = "anova")
 
-  # graph showing how patients are dichotomised
-  pdf(paste0("rpart_", gene_id, "_", surv_event, ".pdf"), onefile = FALSE)
-    rattle::fancyRpartPlot(fit_tree)
-  dev.off()
+  colnames(surv_expr_tb)[1:2] <- c("sample", surv_event)
 
-  decision_values <- fit_tree$splits
-  cut_off <- decision_values[1,4]
+  rpart_tb_list <- lapply(seq_along(gene_ids), function(x){
+    gene_id <- gene_ids[x]
+    print(paste0("Working on: ", gene_id))
+    s_expr <- as.data.frame(surv_expr_tb[,c(surv_event, gene_id)])
+    fit_tree <- rpart::rpart(s_expr, method = "anova")
 
-  high_low <- ifelse(surv_expr_tb$gene >= cut_off, "High", "Low")
-  clin_tb <- dplyr::mutate(.data = clin_df, "{gene_id}_group" := high_low,
-                                            "{gene_id}_log2tpm" := as.numeric(s_expr$gene))
+    if(is.null(fit_tree$splits)){
+      return(NULL)
+    } else {
+    # graph showing how patients are dichotomised
+      pdf(paste0("rpart_", x, "_", surv_event, ".pdf"), onefile = FALSE)
+        rattle::fancyRpartPlot(fit_tree)
+      dev.off()
 
-  return(clin_tb)
+      decision_values <- fit_tree$splits
+      cut_off <- decision_values[1,4]
+
+      high_low <- ifelse(unlist(surv_expr_tb[gene_id]) >= cut_off, "High", "Low")
+      gene_tb <- dplyr::mutate(.data = clin_df[],
+                               "{gene_id}_group" := high_low,
+                               "{gene_id}_log2tpm" := as.numeric(unlist(s_expr[x]))) %>%
+                 dplyr::select(patient, barcode,
+                               tidyselect::starts_with(!!as.vector(gene_id)))
+     return(gene_tb)
+    }
+  })
+
+  names(rpart_tb_list) <- gene_ids
+
+  ##remove NULL
+  rpart_tb_list <- rpart_tb_list[!sapply(rpart_tb_list, is.null)]
+  rpart_tb <- rpart_tb_list %>% purrr::reduce(dplyr::left_join) %>%
+                                dplyr::left_join(., clin_df)
+
+  return(rpart_tb)
 }
