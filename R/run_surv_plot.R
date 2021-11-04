9f796987fdb06ef43e6e663da6c906c1c59abf0f
brucemoran <bruce01campus@gmail.com>
2021-11-04 17:14:44 +0000

    text = element_text(size = 25)

--- R/run_surv_plot.R
+++ R/run_surv_plot.R
@@ -63,7 +63,8 @@ run_surv_plot <- function(clin_tb, gene_ids, surv_event, surv_time, expr_unit = 
                                    pval.size = 5,
                                    font.legend = c(10, "plain", "black"),
                                    palette = col_palette,
-                                   title = paste0(title_text, " - ", gene_id, "\n", sub_text))
+                                   title = paste0(title_text, " - ", gene_id, "\n", sub_text)) +
+            ggplot2::theme(text = element_text(size = 25))
 
       ##outputs
       if(!is.null(print_pdf)){
